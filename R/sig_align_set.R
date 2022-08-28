0550de176d12161cbfd860b19458397db9034dea
heike <hofmann@iastate.edu>
2022-08-28 15:58:46 -0500

    documentation, expanded example for tool 1

--- R/sig_align_set.R
+++ R/sig_align_set.R
@@ -14,8 +14,22 @@
 #' @importFrom stats lag
 #' @examples
 #' load("data/toolmarks.Rdata")
-#' long <- sig_align_set(filter(toolmarks, tool == 1, side=="A"), value = signature,  group = mark)
-#' long %>% ggplot(aes(x =x, y = aligned, colour = factor(mark))) + geom_line()
+#' toolmarks %>% group_by(tool, side, size) %>% summarize(angles = length(unique(angle)))
+#' tool1AL <- dplyr::filter(toolmarks, tool == "01", side=="A", size=="L")
+#' long <- tool1AL %>% group_by(angle) %>% nest()
+#' long <- long %>% mutate(
+#'   aligned = data %>% purrr::map(.f = function(d) {
+#'     d %>% sig_align_set(value = signature,  group = mark)
+#'   })
+#' )
+#' long$aligned[[1]] %>% ggplot(aes(x =x, y = aligned, colour = factor(mark))) + geom_line()
+#'
+#' long <- long %>% select(-data) %>% unnest(col=aligned)
+#' long %>%
+#'   ggplot(aes(x =x, y = aligned, colour = factor(mark))) +
+#'   geom_line() +
+#'   facet_grid(angle~., labeller="label_both")
+#'
 sig_align_set <- function(data, value, group) {
   group <- enquo(group)
   value <- enquo(value)
@@ -25,7 +39,7 @@ sig_align_set <- function(data, value, group) {
   dlist <- dlist %>% mutate(
     aligned = data %>% purrr::map(.f = function(d) {
       aligned <- bulletxtrctr::sig_align(dlist$data[[1]] %>% select(!!value) %>% pull,
-                              d %>% select(!!value) %>% pull)
+                                         d %>% select(!!value) %>% pull)
       aligned$lands %>% mutate(
         lag = aligned$lag
       ) %>% select(x, lag, aligned = sig2)
